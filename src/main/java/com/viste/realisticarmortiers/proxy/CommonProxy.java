aea862a7b9bde8f452447698ab292e8e4be3ff39
Isak Viste <isakviste@gmail.com>
2017-06-02 17:10:32 +0200

    Hotfix #1

--- src/main/java/com/viste/realisticarmortiers/proxy/CommonProxy.java
+++ src/main/java/com/viste/realisticarmortiers/proxy/CommonProxy.java
@@ -1,13 +1,13 @@
 package com.viste.realisticarmortiers.proxy;
 
-import com.viste.realisticarmortiers.events.EventEquipementSets;
+import com.viste.realisticarmortiers.events.EventEquipmentSets;
 
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class CommonProxy implements IProxy {
 	
 	public void preInit() {
-		FMLCommonHandler.instance().bus().register(new EventEquipementSets());
+		FMLCommonHandler.instance().bus().register(new EventEquipmentSets());
 	}
 
 	public void init() {
