ce6632827f16c90b6849e41bac53f2f99f1ee533
Isak Viste <isakviste@gmail.com>
2017-06-07 05:51:51 +0200

    Changed JSON file setup, working version 1.0.1

--- src/main/java/com/viste/realisticarmortiers/Reference.java
+++ src/main/java/com/viste/realisticarmortiers/Reference.java
@@ -4,13 +4,18 @@ public class Reference {
 	
 	public static final String MODID = "realisticarmortiers";
 	public static final String NAME = "Realistic Armor Tiers";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	
 	public static final String CLIENT_PROXY = "com.viste.realisticarmortiers.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.viste.realisticarmortiers.proxy.CommonProxy";
 	
-	public static final String JSON_FILE = "equipment_list.json";
-	public static final String ASSET_PATH = "/assets/" + MODID + "/" + JSON_FILE;
 	public static final String CONFIG_PATH = "/RealisticArmorTiers/";
-	public static final String JSON_CONFIG_PATH = CONFIG_PATH + JSON_FILE;
+	
+	public static final String JSON_TIERS_FILE = "equipment_tiers.json";
+	public static final String ASSET_TIERS_PATH = "/assets/" + MODID + "/" + JSON_TIERS_FILE;	
+	public static final String JSON_CONFIG_TIERS_PATH = CONFIG_PATH + JSON_TIERS_FILE;
+	
+	public static final String JSON_SETS_FILE = "equipment_sets.json";
+	public static final String ASSET_SETS_PATH = "/assets/" + MODID + "/" + JSON_SETS_FILE;
+	public static final String JSON_CONFIG_SETS_PATH = CONFIG_PATH + JSON_SETS_FILE;
 }
--- src/main/java/com/viste/realisticarmortiers/events/EventEquipmentSets.java
+++ src/main/java/com/viste/realisticarmortiers/events/EventEquipmentSets.java
@@ -29,7 +29,7 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 public class EventEquipmentSets {	
 	
 	public EventEquipmentGlobalVar global;
-	public List<Armours> armours = new ArrayList<Armours>();
+	public List<Armors> armors = new ArrayList<Armors>();
 	
 	private static final Logger log = LogManager.getLogger(Reference.MODID);
 	
@@ -37,12 +37,14 @@ public class EventEquipmentSets {
 		
 		this.global = new EventEquipmentGlobalVar();
 		
-		// Check if JSON file exists or copy it in
+		// Check / Copy the config files into the config folder
 		File configDir = new File(new String(RealisticArmorTiers.instance.configFile.getPath() + Reference.CONFIG_PATH));
-		File jsonConfig = new File(new String(RealisticArmorTiers.instance.configFile.getPath() + Reference.JSON_CONFIG_PATH));
+		File jsonConfigTiers = new File(new String(RealisticArmorTiers.instance.configFile.getPath() + Reference.JSON_CONFIG_TIERS_PATH));
+		File jsonConfigSets = new File(new String(RealisticArmorTiers.instance.configFile.getPath() + Reference.JSON_CONFIG_SETS_PATH));
 		
+		// Create config folder
 		if(!configDir.exists()) {
-			log.info("(Config Folder) Creating");
+			log.info("(Config Folder) Creating " + Reference.CONFIG_PATH);
 			try {
 				configDir.mkdir();
 			} catch (SecurityException se) {
@@ -50,16 +52,130 @@ public class EventEquipmentSets {
 				log.fatal(se);
 				return;
 			}
-			log.info("(Config Folder) Creation Sucess");
+			log.info("(Config Folder) Creation Success");
+		} else {
+			log.info("(Config Folder) Found " + Reference.CONFIG_PATH);
 		}
+		
+		// Create TIERS & SETS config
+		copyFile(jsonConfigTiers, Reference.ASSET_TIERS_PATH);
+		copyFile(jsonConfigSets, Reference.ASSET_SETS_PATH);
+		
+		// JSON File loading
+		try {
+			log.info("(JSON File) Loading");
 			
-		if(!jsonConfig.exists()) {
-			log.info("(JSON File) Copy to Config folder");
+			List<Tiers> tiers;
+			List<Sets> sets;
+			
+			log.info("-> (File Read) Reading JSON files");
 			try {
-				jsonConfig.createNewFile();
+				// Tiers
+				Gson gsonTiers = new Gson();
+				BufferedReader brTiers = new BufferedReader(new FileReader(new String(RealisticArmorTiers.instance.configFile.getPath() + Reference.JSON_CONFIG_TIERS_PATH)));
+				Type typeTiers = new TypeToken<List<Tiers>>(){}.getType();
+				tiers = gsonTiers.fromJson(brTiers, typeTiers);
 				
-				InputStream instream = this.getClass().getResourceAsStream(Reference.ASSET_PATH);
-				FileOutputStream outstream = new FileOutputStream(jsonConfig);
+				// Sets
+				Gson gsonSets = new Gson();
+				BufferedReader brSets = new BufferedReader(new FileReader(new String(RealisticArmorTiers.instance.configFile.getPath() + Reference.JSON_CONFIG_SETS_PATH)));
+				Type typeSets = new TypeToken<List<Sets>>(){}.getType();
+				sets = gsonSets.fromJson(brSets, typeSets);
+			} catch (IOException ioe) {
+				log.fatal("-> (File Read) Reading Failure");
+				log.fatal(ioe);
+				return;
+			}
+			
+			log.info("-> (File Read) Reading Success");
+			
+			try {
+				log.info("-> (Armors) Loading All");
+				for(int i = 0; i < sets.size(); i++) {
+					float speed = 0;
+					boolean found = false;
+					for(int j = 0; j < tiers.size(); j++) {
+						if(sets.get(i).set.contentEquals(tiers.get(j).set)) {
+							speed = tiers.get(j).speed;
+							found = true;
+							break;
+						}
+					}
+					
+					if(found == false) {
+						log.warn("-> -> (Armor Set) " + sets.get(i).set + " was not found in " + Reference.JSON_TIERS_FILE);
+					}
+					
+					List<Effects> effects = sets.get(i).effects;
+					List<ItemArmor> piecesArmors = new ArrayList<ItemArmor>();
+					for(int j = 0; j < 4; j++) {
+						String path = sets.get(i).modId + ":" + sets.get(i).pieces.get(j).toLowerCase();
+						ItemArmor armor = (ItemArmor)ItemArmor.getByNameOrId(path);
+						if(armor == null) {
+							log.warn("-> -> (Armor Piece) " + sets.get(i).pieces.get(j) + " was not found!");
+						} else {
+							piecesArmors.add(armor);
+						}
+					}
+					armors.add(new Armors(global, piecesArmors, effects, speed));
+				}
+				log.info("-> (Armors) Loading Success");
+			} catch (Exception e) {
+				log.fatal("-> (Armors) Loading Failure");
+				log.fatal(e);
+			}
+		} catch (Exception e) {
+			log.fatal("(JSON File) Loading Failure");
+			log.fatal(e);
+			return;
+		}
+		log.info("(JSON File) Loading Success");
+	}
+
+	@SubscribeEvent
+	public void onArmorUpdate(PlayerTickEvent evt) {
+		
+		Item armor = null;
+		ItemStack stacks = null;
+		
+		int j=0;
+		
+		global.setSpeed(0.1f);
+		
+		while (j < armors.size()){
+			armors.get(j).resetPieces();
+			j++;
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			stacks = evt.player.inventory.armorItemInSlot(i);
+			
+			if(stacks != null) {
+				armor = stacks.getItem();
+				
+				j=0;
+				while (j < armors.size()){
+					if(armors.get(j).checkArmor(armor,evt)){
+						break;
+					}
+					
+					j++;
+				}
+			}
+		}
+		// Movement Speed
+		evt.player.capabilities.setPlayerWalkSpeed(global.getSpeed());
+	}
+	
+	// Copy from 'asset' into 'file'
+	void copyFile(File file, String asset) {
+		if(!file.exists()) {
+			log.info("(JSON File) Copying " + asset);
+			try {
+				file.createNewFile();
+				
+				InputStream instream = this.getClass().getResourceAsStream(asset);
+				FileOutputStream outstream = new FileOutputStream(file);
 				
 				byte[] buffer = new byte[1024];
 				int length;
@@ -76,118 +192,48 @@ public class EventEquipmentSets {
 				return;
 			}
 			log.info("(JSON File) Copy Success");
+		} else {
+			log.info("(JSON File) Found " + asset); 
 		}
-		
-		// JSON File loading
-		Gson gson = new Gson();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(new String(RealisticArmorTiers.instance.configFile.getPath() + Reference.JSON_CONFIG_PATH)));
-			Type type = new TypeToken<List<JsonModel>>(){}.getType();
-			log.info("(JSON File) Loading");
-			List<JsonModel> models = gson.fromJson(br, type);
-			try {
-				log.info("(Armors) Loading All");
-				for(int i = 0; i < models.size(); i++) {
-					List<Effects> effects = models.get(i).effects;
-					List<ItemArmor> piecesArmors = new ArrayList<ItemArmor>();
-					for(int j = 0; j < 4; j++) {
-						try {
-							String path = models.get(i).modId + ":" + models.get(i).pieces.get(j).toLowerCase();
-							ItemArmor armor = (ItemArmor)ItemArmor.getByNameOrId(path);
-							piecesArmors.add(armor);
-						} catch (Exception e) {
-							log.fatal("(Armor Piece) Loading Failure");
-							log.fatal(e);
-						}
-					}
-					armours.add(new Armours(global, piecesArmors, effects, models.get(i).speed));
-				}
-				log.info("(Armors) Loading Sucess");
-			} catch (Exception e) {
-				log.fatal("(Armors) Loading Failure");
-				log.fatal(e);
-			}
-			
-			
-		} catch (Exception e) {
-			log.fatal("(JSON File) Loading Failure");
-			log.fatal(e);
-			return;
-		}
-		log.info("(JSON File) Loading Sucess");
-	}
-
-	@SubscribeEvent
-	public void onArmorUpdate(PlayerTickEvent evt) {
-		
-		Item armor = null;
-		ItemStack stacks = null;
-		
-		int j=0;
-		
-		global.setSpeed(0.1f);
-		
-		while (j < armours.size()){
-			armours.get(j).resetPieces();
-			j++;
-		}
-		
-		for(int i = 0; i < 4; i++) {
-			stacks = evt.player.inventory.armorItemInSlot(i);
-			
-			if(stacks != null) {
-				armor = stacks.getItem();
-				
-				j=0;
-				while (j < armours.size()){
-					if(armours.get(j).checkArmour(armor,evt)){
-						break;
-					}
-					
-					j++;
-				}
-			}
-		}
-		// Movement Speed
-		evt.player.capabilities.setPlayerWalkSpeed(global.getSpeed());
 	}
 }
 
-class Armours {
-	private List<ItemArmor> armours = null;
-	private int armourPieces = 0;
+class Armors {
+	private List<ItemArmor> armors = null;
+	private int armorPieces = 0;
 	private List<Effects> effects;
 	private EventEquipmentGlobalVar global;
 	private float speed = 0;
 	
-	public Armours(EventEquipmentGlobalVar global, List<ItemArmor> items, List<Effects> effects, float speed) {
+	public Armors(EventEquipmentGlobalVar global, List<ItemArmor> items, List<Effects> effects, float speed) {
 		this.global = global;
 		this.speed = speed;
-		this.armours = items;
+		this.armors = items;
 		this.effects = effects;
 	}
-	public boolean checkArmour(Item item, PlayerTickEvent evt){
-		if(armours.contains(item)){
-			this.armourPieces++;
+	
+	public boolean checkArmor(Item item, PlayerTickEvent evt){
+		if(armors.contains(item)){
+			this.armorPieces++;
 			global.setSpeed(global.getSpeed() + speed);
-			this.addSetEffectsArmour(evt);
+			this.addSetEffectsArmor(evt);
 			return true;
 		}
 		return false;
 	}
 
 	public boolean isFullSet(){
-		if(this.armourPieces >= 4){
+		if(this.armorPieces >= 4){
 			return true;
 		}
 		return false;
 	}
 
 	public void resetPieces(){
-		this.armourPieces = 0;
+		this.armorPieces = 0;
 	}
 
-	public void addSetEffectsArmour(PlayerTickEvent evt){
+	public void addSetEffectsArmor(PlayerTickEvent evt){
 		if(this.isFullSet()){
 			int i = 0;
 			while (i < effects.size()){
@@ -198,19 +244,17 @@ class Armours {
 	}
 }
 
-class JsonModel {
-	public String name;
+class Sets {
+	public String set;
 	public String modId;
 	public List<String> pieces;
 	public List<Effects> effects;
-	public float speed;
 	
-	public JsonModel(String name, String modId, List<String> pieces, List<Effects> effects, float speed) {
-		this.name = name;
+	public Sets(String set, String modId, List<String> pieces, List<Effects> effects) {
+		this.set = set;
 		this.modId = modId;
 		this.pieces = pieces;
 		this.effects = effects;
-		this.speed = speed;
 	}
 }
 
@@ -223,6 +267,19 @@ class Effects {
 		this.efficiency = efficiency;
 	}
 }
+
+class Tiers {
+	public String set;
+	public int weight;
+	public float speed;
+	
+	public Tiers(String set, int weight, float speed) {
+		this.set = set;
+		this.weight = weight;
+		this.speed = speed;
+	}
+}
+
 class EventEquipmentGlobalVar {
 	private int maxPotions = 28; // Amount of potions + 1
 	private int potionDur = 20; // 20 ticks ~= 1 second
@@ -231,6 +288,7 @@ class EventEquipmentGlobalVar {
 	public float getSpeed(){
 		return this.newPlayerSpeed;
 	}
+	
 	public void setSpeed(float speed){
 		this.newPlayerSpeed = speed;
 	}
