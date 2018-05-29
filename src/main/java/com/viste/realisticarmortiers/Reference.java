80e70b0d57c04cbb71ee018b8c1cf873c6f21e5f
Isak Viste <isakviste@gmail.com>
2018-05-29 15:38:40 +0200

    Updated to 1.1.1

--- src/main/java/com/viste/realisticarmortiers/Reference.java
+++ src/main/java/com/viste/realisticarmortiers/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MODID = "realisticarmortiers";
 	public static final String NAME = "Realistic Armor Tiers";
 	public static final String SHORT_NAME = "ArmorTiers";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 	
 	public static final String CLIENT_PROXY = "com.viste.realisticarmortiers.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.viste.realisticarmortiers.proxy.CommonProxy";
